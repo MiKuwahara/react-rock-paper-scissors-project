a7e45273dc689202f5428962ee8b75cfb7007e7a
Michelle Kuwahara <mxk200037@utdallas.edu>
2023-09-22 23:14:48 -0500

    Replace game moves with images and add styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
+/**
+ * Sources: JavaScript Full Course (2023) - Beginner to Pro - Part 1 by SuperSimpleDev
+ * Goal: To convert a plain js rock-paper-scissors to a react rock-paper-scissors
+ * Purpose: To practice my react skills
+ * **/
 
+
+import rock from './rock-emoji.png';
+import paper from './paper-emoji.png';
+import scissors from './scissors-emoji.png';
 import {useState} from 'react';
 import './App.css';
 
@@ -6,23 +15,25 @@ import './App.css';
 function Option({value, onOptionClick}){
 
   return (
-    <button className="option" onClick={onOptionClick}>{value}</button>
+    <button className="option" onClick={onOptionClick}><img className='move-icon' src={value} alt='move-icon'></img></button>
   )
 }
 
 export default function Game() {
   const [playerMove, setPlayerMove] = useState(Array(2).fill(null));
   const [gameState, setGameState] = useState(null);
-
+  const [userIcon, setUserIcon] = useState(null);
+  const [computerIcon, setComputerIcon] = useState(null);
 
   function handleClick(userPick){
     const nextPlayers = playerMove.slice();
     nextPlayers[0] = userPick;
-
     nextPlayers[1] = pickComputerMove();
     setPlayerMove(nextPlayers);
+
   
     if(nextPlayers[0] === "rock"){
+      setUserIcon(rock);
       if(nextPlayers[1] === "rock"){
         setGameState("It's a tie!");
       }else if(nextPlayers[1] === "paper"){
@@ -31,6 +42,7 @@ export default function Game() {
         setGameState("You win!");
       }
     }else if(nextPlayers[0] === "paper"){
+      setUserIcon(paper);
       if(nextPlayers[1] === "rock"){
         setGameState("You win!");
       }else if(nextPlayers[1] === "paper"){
@@ -39,6 +51,7 @@ export default function Game() {
         setGameState("You lose!");
       }
     }else{
+      setUserIcon(scissors);
       if(nextPlayers[1] === "rock"){
         setGameState("You lose!");
       }else if(nextPlayers[1] === "paper"){
@@ -47,18 +60,26 @@ export default function Game() {
         setGameState("It's a tie!");
       }
     }
+
+    if(nextPlayers[1] === "rock"){
+      setComputerIcon(rock);
+    }else if(nextPlayers[1] === "paper"){
+      setComputerIcon(paper);
+    }else{
+      setComputerIcon(scissors);
+    }
     
   } 
 
   return (
     <div className="App">
-      <p>Rock Paper Scissors</p>
-      <Option value="Rock"    onOptionClick={() => handleClick("rock")}/>
-      <Option value="Paper"   onOptionClick={() => handleClick("paper")}/>
-      <Option value="Scissor" onOptionClick={() => handleClick("scissors")}/>
-      <p>User: {playerMove[0]}</p>
-      <p>Computer: {playerMove[1]}</p>
-      <p>Result: {gameState}</p>
+      <p className="title">Rock Paper Scissors</p>
+      <Option value={rock}    onOptionClick={() => handleClick("rock")}/>
+      <Option value={paper}   onOptionClick={() => handleClick("paper")}/>
+      <Option value={scissors} onOptionClick={() => handleClick("scissors")}/>
+      <p className="result">{gameState}</p>
+      <p>User: <img className="players" src={userIcon}></img></p>
+      <p>Computer: <img className="players" src={computerIcon}></img></p>
     </div>
   );
 }
